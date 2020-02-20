facc8c522e11e268315f94bae769115c9421593a
unknown <world4den@mail.ru>
2020-02-21 02:54:26 +0600

    adaptive v0.9 + roadmap fix. todo:adaptive, anims, pages

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('#header-slider', {
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.header-slider-pagination',
+        bulletClass:'pagination-bullet',
+        bulletActiveClass: 'pagination-bullet-active',
+        clickable: true,
+      },
+});
